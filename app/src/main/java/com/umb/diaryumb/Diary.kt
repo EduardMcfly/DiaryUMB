cb2667766895316c626c453e0c0c9994943dd297
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-02-26 10:22:29 -0500

    chore: data class Diary added

--- app/src/main/java/com/umb/diaryumb/Diary.kt
+++ app/src/main/java/com/umb/diaryumb/Diary.kt
@@ -1,3 +1,13 @@
 package com.umb.diaryumb
 
-data class Diary(val id: Int, val date: String )
+data class Diary(
+    val id: Int,
+    val date: String,
+    val affair: String,
+    val activity: String,
+    var selected: Boolean = true
+) {
+    fun isSelected(): Boolean {
+        return selected
+    }
+}
